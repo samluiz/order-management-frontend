9a2fb415a8c1d23e302cc5327ba9f009cd73de9c
Samuel Luiz <samuelluiz@ibm.com>
2024-03-04 14:18:33 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <div class="wrapper">
+      <header>
+        <nav class="">
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/products">Products</RouterLink>
+          <RouterLink to="/orders">Orders</RouterLink>
+        </nav>
+      </header>
+    </div>
+  <RouterView />
+</template>
